df4a574171dc594127121d3cd26384e9a75fc5a7
jaspreet1388 <chha0038@algonquinlive.com>
2025-05-11 12:35:10 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,11 @@
+# app.py
+from fastapi import FastAPI
+import time, random
+app = FastAPI()
+
+@app.get("/health")
+def health_check():
+    delay = random.uniform(0.1, 1.0)
+    time.sleep(delay)
+    return {"status": "ok", "latency": delay}
 
